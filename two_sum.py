06b51c434a0e33c3a33e21886d5f84c58a13f3e0
DataWizard-Yash <yashbhatt.datascience@gmail.com>
2023-08-02 00:51:03 +0530

    Two Sum problem Leetcode Python Solution

---
+++ two_sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash_map = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hash_map:
+                return [hash_map[diff], i]
+            else:
+                hash_map[n] = i
+        return
